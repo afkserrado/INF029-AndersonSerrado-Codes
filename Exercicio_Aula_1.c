9d632ea781112dcbecad80e11df2aecf1e6cf557
AndersonSerrado <akserrado@gmail.com>
2025-04-12 18:47:56 -0300

    Exclusão de animais

--- Exercicio_Aula_1.c
+++ Exercicio_Aula_1.c
@@ -7,33 +7,34 @@ Ainda, deve ser possível:
 
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #define tam 10
 #define tamNome 100
 #define tamData 10 //Formato DD/MM/AAAA
 
+//Registro para datas
+typedef struct{
+    int dia;
+    int mes;
+    int ano;
+} Data;
+
+//Registro para armazenar os dados dos animais
+typedef struct{
+    int id;
+    int idade;
+    char sexo;
+    char nome[tamNome];
+    Data nascimento;
+} listaAnimais;
+
 int main(){
-    
-    //Registro para datas
-    struct Data{
-        int dia;
-        int mes;
-        int ano;
-    };
-    
-    //Registro para armazenar os dados dos animais
-    struct Animais{
-        int idade[tam];
-        char sexo[tam];
-        char nome[tam][tamNome];
-        struct Data nascimento[tam];
-    };
 
     //Variável Animal da struct Animais
-    struct Animais Animal;
+    listaAnimais Animal[tam];
 
     //Declarações
-    int i, cont = 0, soma = 0, media = 0, opcao;
-    char data_aux[tamData];
+    int i, cont = 0, soma, media, opcao, matricula, incrementadorMatricula = 0;
 
     //While 1
     while (cont < tam){
@@ -42,6 +43,7 @@ int main(){
         printf("\nInforme 1 para cadastrar um animal. ");
         printf("\nInforme 2 para listar todos os animais.");
         printf("\nInforme 3 para listar todos os animais com idade acima da média.");
+        printf("\nInforme 4 para excluir um animal.");
         printf("\nInforme 0 para encerrar.\n");
         scanf("%d", &opcao);
 
@@ -49,55 +51,119 @@ int main(){
         switch(opcao){
             //Cadastrar animais
             case 1:
-                getchar(); //Limpar o '\n' do buffer
-                
-                //Nome
-                printf("\nInforme o nome do animal: ");
-                fgets(Animal.nome[cont], sizeof(Animal.nome[cont]), stdin);
-                Animal.nome[cont][strcspn(Animal.nome[cont], "\n")] = '\0';
-                
-                //Data de nascimento
-                printf("\nInforme a data de nascimento no formato DD/MM/AAAA: ");
-                scanf(" %s", data_aux);
-                
-                //Idade
-                printf("\nInforme a idade do animal: ");
-                scanf("%d", &Animal.idade[cont]);
-                
-                //Sexo
-                printf("\nInforme o sexo do animal: ");
-                scanf(" %c", &Animal.sexo[cont]);
-                Animal.sexo[cont] = toupper(Animal.sexo[cont]);
-
-                //Separando a data
-                Animal.nascimento[cont].dia = (data_aux[0] - '0') * 10 + (data_aux[1] - '0');
-                Animal.nascimento[cont].mes = (data_aux[3] - '0') * 10 + (data_aux[4] - '0');
-                Animal.nascimento[cont].ano = (data_aux[6] - '0') * 1000 + (data_aux[7] - '0') * 100 + (data_aux[8] - '0') * 10 + (data_aux[9] - '0');
-                
-                //Calculando a média
-                soma += Animal.idade[cont];
-                media = soma / (cont + 1);
-
-                //Contador
-                cont++;
+                if (cont < tam){
+                    getchar(); //Limpar o '\n' do buffer
+                    
+                    //ID
+                    Animal[cont].id = ++incrementadorMatricula;
+                    
+                    //Nome
+                    printf("\nInforme o nome do animal: ");
+                    fgets(Animal[cont].nome, sizeof(Animal[cont].nome), stdin);
+                    Animal[cont].nome[strcspn(Animal[cont].nome, "\n")] = '\0';
+                    
+                    //Data de nascimento
+                    printf("\nInforme a data de nascimento no formato DD/MM/AAAA: ");
+                    scanf("%d/%d/%d", &Animal[cont].nascimento.dia, &Animal[cont].nascimento.mes, &Animal[cont].nascimento.ano);
+                    
+                    //Idade
+                    printf("\nInforme a idade do animal: ");
+                    scanf("%d", &Animal[cont].idade);
+                    
+                    //Sexo
+                    printf("\nInforme o sexo do animal: ");
+                    scanf(" %c", &Animal[cont].sexo);
+                    Animal[cont].sexo = toupper(Animal[cont].sexo);
+    
+                    //Contador
+                    cont++;
+                }
+                else
+                    printf("\nPET cheio.");
             break;
 
             //Listar animais
             case 2:
                 for (i = 0; i < cont; i++){
-                    printf("\nNome: %s / Data de nascimento: %02d/%02d/%04d / Idade: %d / Sexo: %c", Animal.nome[i], Animal.nascimento[i].dia, Animal.nascimento[i].mes, Animal.nascimento[i].ano, Animal.idade[i], Animal.sexo[i]);
+                    printf("\nID: %d ## Nome: %s ## Data de nascimento: %02d/%02d/%04d ## Idade: %d ## Sexo: %c", Animal[i].id, Animal[i].nome, Animal[i].nascimento.dia, Animal[i].nascimento.mes, Animal[i].nascimento.ano, Animal[i].idade, Animal[i].sexo);
                 }
                 printf("\n");
             break;
             
             //Listar animais com idade acima da média
             case 3:
-                for (i = 0; i < cont; i++){
-                    if (Animal.idade[i] > media){
-                        printf("\nNome: %s / Data de nascimento: %02d/%02d/%04d / Idade: %d / Sexo: %c", Animal.nome[i], Animal.nascimento[i].dia, Animal.nascimento[i].mes, Animal.nascimento[i].ano, Animal.idade[i], Animal.sexo[i]);
-                    }
-                printf("\n");
+                //Calculando a média
+                soma = 0;
+                media = 0;
+
+                if (cont == 0){
+                    printf("\nNão há animais cadastrados.");
+                    break;
                 }
+                    
+                for (i = 0; i < cont; i++){
+                    soma += Animal[i].idade;
+                }
+                media = soma / cont;
+
+                for (i = 0; i < cont; i++){
+                    if (Animal[i].idade > media){
+                        printf("\nID: %d ## Nome: %s ## Data de nascimento: %02d/%02d/%04d ## Idade: %d ## Sexo: %c", 
+                            Animal[i].id,
+                            Animal[i].nome, 
+                            Animal[i].nascimento.dia, 
+                            Animal[i].nascimento.mes, 
+                            Animal[i].nascimento.ano, 
+                            Animal[i].idade, 
+                            Animal[i].sexo);
+                    }
+                }
+                printf("\n");
+            break;
+
+            //Excluir animais
+            case 4:
+                //Verifica se há animais cadastrados
+                if (cont > 0){
+                    //Lista os animais cadastrados
+                    for (i = 0; i < cont; i++){
+                        printf("\nID: %d ## Nome: %s ## Data de nascimento: %02d/%02d/%04d ## Idade: %d ## Sexo: %c", 
+                            Animal[i].id, 
+                            Animal[i].nome, 
+                            Animal[i].nascimento.dia, 
+                            Animal[i].nascimento.mes, 
+                            Animal[i].nascimento.ano, 
+                            Animal[i].idade, 
+                            Animal[i].sexo);
+                    }
+                    printf("\n");
+                    
+                    //Entrada de dados: matrícula a ser excluída
+                    printf("\nInforme o ID do animal a ser excluído: ");
+                    scanf("%d", &matricula);
+                    int achou = 0; //Flag
+
+                    //Percorrendo o vetor até encontrar uma matrícula coincidente
+                    for (i = 0; i < cont; i++){
+                        if (matricula == Animal[i].id){
+                            achou = 1;
+                            
+                            //Shift
+                            for (int j = i; j < cont; j++){
+                                Animal[j] = Animal[j + 1];
+                            }
+                            printf("\nExclusão realizada com sucesso!");
+                            cont--;
+                            break;
+                        }
+                    }
+                    if (!achou)
+                        printf("\nNão foi encontrado nenhum animal com essa matrícula.");
+
+                    printf("\n");
+                }
+                else
+                    printf("\nNão há animais cadastrados.");
             break;
              
             //Encerrar programa  
