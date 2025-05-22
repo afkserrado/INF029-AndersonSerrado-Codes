34815d729ce516f7613fb47a87758dcc7adcfad4
afkserrado <akserrado@gmail.com>
2025-05-22 02:16:32 -0300

    UPD

--- AndersonSerrado-20242160026-T1/AndersonSerrado-20242160026-T1-Q8.c
+++ AndersonSerrado-20242160026-T1/AndersonSerrado-20242160026-T1-Q8.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void iniciaMatriz (int lin, int col, char matriz[lin][col]) {
+#define lin 3
+#define col 3
+
+void iniciaMatriz (char matriz[lin][col]) {
     for (int i = 0; i < lin; i++){
         for (int j = 0; j < col; j++){
             matriz[i][j] = ' ';
@@ -9,7 +13,7 @@ void iniciaMatriz (int lin, int col, char matriz[lin][col]) {
     }
 }
 
-void exibeMatriz (int lin, int col, char matriz[lin][col]) {
+void exibeMatriz (char matriz[lin][col]) {
     
     char idLin = 'A';
 
@@ -48,20 +52,68 @@ void exibeMatriz (int lin, int col, char matriz[lin][col]) {
     printf("\n");
 }
 
+// Converte de char para int e verifica se os valores são válidos
+int testes (char celula[], int *idLin, int *idCol, char valor) {
+
+    // Converte de char para int
+    *idLin = celula[0] - 'A' + 1;
+    *idCol = celula[1] - '0';
+
+    // Valida a célula
+    if (*idLin < 1 || *idLin > 3 || *idCol < 1 || *idCol > 3) {
+        printf("\nCélula inválida. Tente novamente...");
+        return -1;
+    }
+
+    // Valida o valor
+    if (valor != 'X' && valor != 'O') {
+        printf("\nValor inválido. Tente novamente...");
+        return -1;
+    }
+
+    return 1;
+}
+
 int main () {
 
-    int lin = 3;
-    int col = 3;
     char matriz[lin][col];
     char celula[3]; // +1 para o \0
+    char valor;
+    int idLin, idCol;
 
-    iniciaMatriz(lin, col, matriz);
+    // Preenche a matriz com espaços em branco
+    iniciaMatriz(matriz);
 
+    printf("\nPara jogar, utilize apenas letras capitalizadas:");
+    printf("\nA, B ou C para identificar as linhas.");
+    printf("\nX ou O para identificar atribuir um valor.\n");
+
+    int jogador = 1;
     while (1) {
+        // Entrada de dados: célula
+        printf("\nVez do jogador %d", jogador);
         printf("\nInforme a célula: ");
         fgets(celula, sizeof(celula), stdin);
         celula[strcspn(celula, "\n")] = '\0';
-    }
 
-    exibeMatriz(lin, col, matriz);
+        // Entrada de dados: valor
+        printf("\nInforme o valor (X ou O): ");
+        scanf(" %c", &valor);
+        
+        // Preenche a matriz
+        if (testes(celula, &idLin, &idCol, valor)) { // return = 1
+            matriz[idLin][idCol] = valor;
+            exibeMatriz(matriz);
+
+            // Muda o jogador
+            if (jogador == 1) {jogador++;}
+            else {jogador--;}
+        }
+        else {
+            printf("\n");
+        }
+
+    }
+    
+    
 }
